51f7583ffaf968f47bd421d9d3a3deba51783f78
Riadh <gharbiriadh16@gmail.com>
2025-05-11 10:41:35 +0100

    update FlutterSecurePlugin to handle encryption and decryption errors with improved exception handling

--- lib/flutter_secure_plugin.dart
+++ lib/flutter_secure_plugin.dart
@@ -3,13 +3,31 @@ import 'package:flutter/services.dart';
 class FlutterSecurePlugin {
   static const MethodChannel _channel = MethodChannel('flutter_secure_plugin');
 
-  static Future<String> encrypt(String plainText) async {
-    final String result = await _channel.invokeMethod('encrypt', {'plainText': plainText});
-    return result;
+  static Future<String?> encrypt(String plainText) async {
+    try {
+      final result =
+          await _channel.invokeMethod('encrypt', {'plainText': plainText});
+      return result as String?;
+    } on PlatformException catch (e) {
+      print('Error encrypting: ${e.message}');
+      return null;
+    } catch (e) {
+      print('Unexpected error: $e');
+      return null;
+    }
   }
 
-  static Future<String> decrypt(String encryptedValue) async {
-    final String result = await _channel.invokeMethod('decrypt', {'encryptedValue': encryptedValue});
-    return result;
+  static Future<String?> decrypt(String encryptedValue) async {
+    try {
+      final result = await _channel
+          .invokeMethod('decrypt', {'encryptedValue': encryptedValue});
+      return result as String?;
+    } on PlatformException catch (e) {
+      print('Error decrypting: ${e.message}');
+      return null;
+    } catch (e) {
+      print('Unexpected error: $e');
+      return null;
+    }
   }
 }
